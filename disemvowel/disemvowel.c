81fbd653ed166e6e86e616959ac7850bdb03ba48
JacobJenness <jenne077@morris.umn.edu>
2020-09-18 18:22:30 -0500

    Push disemvowel WIP
    This commit adds another version of disemvowel that is still a WIP

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "disemvowel.h"
 
@@ -8,42 +9,52 @@ char *disemvowel(char *str) {
   int len, i, j, x;
   x=0;
   len = strlen(str);
-  char *tmp;
   char *result;
+  char *tmp;
   tmp = (char*) calloc(50000, sizeof(char));
-  char v[] = {'a','A','e','E','i','I','o','O','u','U','\0'};
-  char *vowels;
-  vowels = v;
 
   //Checks to see if the string contains anything.
   if (len <= 0) {
     return str;
   }
 
-  /*The actual disemvowel process. It will loop
-  through the given string, compare that character
-  with the vowels, and add it to tmp if it's not
-  a vowel.*/
+  //The actual disemvowel process. It will loop
+  //through the given string, see if that character
+  //is a vowel, and add it to tmp if it's not
+  //a vowel.
   for (i=0; i<len; i++) {
-    for (j=0; j<10; j++) {
-      if (str[i] != vowels[j]) {
+      if (str[i] == is_a_vowel(str[i])) {
 	  tmp[x] = str[i];
-	  printf("%d ", str[i]);
 	  x++;
       }
+  }
+
+  //Creates a new array named result that is
+  //significantly smaller than tmp, but contains
+  //the same amount of data.
+  result = (char*) calloc(x+1, sizeof(char));
+  for (i=0; i<=x; i++) {
+    result[i] = tmp[i];
+  }
+  result[x+1] = '\0';
+
+  free(tmp);
+  //free(vowels);
+  return result;
+}
+
+char *is_a_vowel(char *str) {
+  char v[] = {'a','A','e','E','i','I','o','O','u','U'};
+  char *vowels;
+  vowels = v;
+  char *result;
+
+  //Yes, this is inefficient, but I don't know how else to do this
+  for (i=0; i<10; i++) {
+    if (vowels[i] != str[0]) {
+    result[0] = str[0];
     }
   }
 
-  /*Creates a new array named result that is
-  significantly smaller than tmp, but contains
-  the same amount of data.*/
-  result = (char*) calloc(x+1, sizeof(char));
-  for (i=0; i<x; i++) {
-    result[i] = tmp[i];
-  }
-  result[x] = '\0';
-
-  free(tmp);
-  free(vowels);
   return result;
 }
