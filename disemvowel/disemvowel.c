949f25f6b0a41c7da86afdf14f31ecf44b5d8f51
JacobJenness <jenne077@morris.umn.edu>
2020-09-18 16:58:30 -0500

    Push disemvowel WIP
    This commit adds the basic structure of disemvowel that will be tweaked and modified to work perfectly later on.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,49 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  int len, i, j, x;
+  x=0;
+  len = strlen(str);
+  char *tmp;
+  char *result;
+  tmp = (char*) calloc(50000, sizeof(char));
+  char v[] = {'a','A','e','E','i','I','o','O','u','U','\0'};
+  char *vowels;
+  vowels = v;
+
+  //Checks to see if the string contains anything.
+  if (len <= 0) {
+    return str;
+  }
+
+  /*The actual disemvowel process. It will loop
+  through the given string, compare that character
+  with the vowels, and add it to tmp if it's not
+  a vowel.*/
+  for (i=0; i<len; i++) {
+    for (j=0; j<10; j++) {
+      if (str[i] != vowels[j]) {
+	  tmp[x] = str[i];
+	  printf("%d ", str[i]);
+	  x++;
+      }
+    }
+  }
+
+  /*Creates a new array named result that is
+  significantly smaller than tmp, but contains
+  the same amount of data.*/
+  result = (char*) calloc(x+1, sizeof(char));
+  for (i=0; i<x; i++) {
+    result[i] = tmp[i];
+  }
+  result[x] = '\0';
+
+  free(tmp);
+  free(vowels);
+  return result;
 }
