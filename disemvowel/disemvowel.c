aa558fc67470d4f4599a0d38be5a2390b74a48b5
JacobJenness <jenne077@morris.umn.edu>
2020-09-18 19:45:32 -0500

    Push disemvowel WIP
    This commit is the third time that I have pushed a WIP disemvowel.
    This time, it can actually recognize whether a character is a vowel.
    Yay.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,32 +1,64 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 #include "disemvowel.h"
 
+  //Checks to see if the given char is a vowel and returns
+  //a 0 if it is, and a 1 if it is not.
+int notAVowel(char str) {
+  switch (str) {
+    case 'a':
+    case 'A':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+      return 0;
+      break;
+    default:
+      return 1;
+      break;
+  }
+}
+
 char *disemvowel(char *str) {
-  int len, i, j, x;
-  x=0;
-  len = strlen(str);
-  char *result;
-  char *tmp;
-  tmp = (char*) calloc(50000, sizeof(char));
+  int length;
+  int i;
+  int newLength = 0;
+  length = strlen(str);
+  //char *result;
+  //char *tmp;
+  //tmp = (char*) calloc(50000, sizeof(char));
 
   //Checks to see if the string contains anything.
-  if (len <= 0) {
+  //If it doesn't contain anything, return the string.
+  if (length <= 0) {
     return str;
   }
+
+  //Count the number of vowels in str
+  for (i=0; i<length; i++) {
+    if (notAVowel(str[i]) == 1) {
+    newLength++;
+    }
+  }
+//printf("%d\n", newLength);
+//return str;
 
   //The actual disemvowel process. It will loop
   //through the given string, see if that character
   //is a vowel, and add it to tmp if it's not
   //a vowel.
-  for (i=0; i<len; i++) {
-      if (str[i] == is_a_vowel(str[i])) {
-	  tmp[x] = str[i];
-	  x++;
-      }
+  for (i=0; i<length; i++) {
+    if (notAVowel(str[i])==1) {
+      tmp[x] = str[i];
+      x++;
+    }
   }
 
   //Creates a new array named result that is
@@ -39,22 +71,6 @@ char *disemvowel(char *str) {
   result[x+1] = '\0';
 
   free(tmp);
-  //free(vowels);
   return result;
 }
 
-char *is_a_vowel(char *str) {
-  char v[] = {'a','A','e','E','i','I','o','O','u','U'};
-  char *vowels;
-  vowels = v;
-  char *result;
-
-  //Yes, this is inefficient, but I don't know how else to do this
-  for (i=0; i<10; i++) {
-    if (vowels[i] != str[0]) {
-    result[0] = str[0];
-    }
-  }
-
-  return result;
-}
