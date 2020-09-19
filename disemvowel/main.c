a19bed5fb89ae5f4c6aeb8347134922db1a79382
JacobJenness <jenne077@morris.umn.edu>
2020-09-18 21:09:16 -0500

    Final commit
    This commit fixes the memory leak that was occuring in main.c
    and also gives the user the ability to quit the program with 'q'
    All tests for all programs are passing, and there are no memory leaks.

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -5,12 +5,18 @@
 
 int main(int argc, char *argv[]) {
   char *line;
+  char *result;
   size_t size;
   
   size = 100;
   line = (char*) malloc (size + 1);
+  printf("Please enter a string. Use can use q to quit: \n");
 
-  while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+  while (getline(&line, &size, stdin) > 0 && line[0] != 'q') {
+    result = disemvowel(line);
+    printf("%s\n", result);
+    free(result);
   }
+
+  free(line);
 }
